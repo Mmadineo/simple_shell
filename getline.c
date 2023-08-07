a8069b2b18d9a62af94442609d48294262118860
ogolla23 <leonardogolla23@gmail.com>
2023-08-07 09:43:39 -0700

    update 1.4

--- getline.c
+++ getline.c
@@ -120,7 +120,7 @@ return (-1);
 c = _strchr(buf + i, '\n');
 k = c ? 1 + (unsigned int)(c - buf) : len;
 new_p = _realloc(p, s, s ? s + k : k + 1);
-if (!new_p) /* MALLOC FAILURE! */
+if (!new_p)
 return (p ? free(p), -1 : -1);
 if (s)
 _strncat(new_p, buf + i, k - i);
